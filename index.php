37f377d35a4f54dd80204db567630b876c50662e
Student <std@aahn02.edu>
2021-01-30 21:13:17 +0700

    baiAdvan

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CÁC DẠNG BÀI TẬP VỀ CSS</title>
+</head>
+<body>
+	<h1>Bài tập Advan</h1>
+	<br><a href="baiAdvan/TouristAttractions.html">Bài 1</a>
+</body>
+</html>
